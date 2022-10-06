6dc34f1750b1a7df3159fec4f7d6434e124388c5
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-06 19:16:44 +0300

    fix - update components folder structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import { ReactComponent as OfferIcon } from "../../assets/svg/localOfferIcon.svg";
-import { ReactComponent as ExploreIcon } from "../../assets/svg/exploreIcon.svg";
-import { ReactComponent as PersonOutlineIcon } from "../../assets/svg/personOutlineIcon.svg";
+import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
+import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
+import { ReactComponent as PersonOutlineIcon } from "../assets/svg/personOutlineIcon.svg";
 
 const Navbar = () => {
   const navigate = useNavigate();
