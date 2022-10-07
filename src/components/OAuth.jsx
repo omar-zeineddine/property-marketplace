27bac196db8347494aff86cb20a3dcd35da8c068
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-07 19:14:05 +0300

    feat - add google auth on click event

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import googleIcon from "../assets/svg/googleIcon.svg";
@@ -9,7 +9,29 @@ const OAuth = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const onGoogleClick = () => {};
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // check if user is already in firestore
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      // if user doesn't exist, create one
+      if (!docSnap.exists()) {
+        await setDoc(doc(db, "users", user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (err) {
+      toast.error("could not authorize with Google");
+    }
+  };
 
   return (
     <div className="socialLogin">
