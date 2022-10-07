6d6f9e175d3ba8eeadb82ce1745de9b824d39ce1
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-07 18:48:28 +0300

    feat - add forgot password component

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+
 const ForgotPassword = () => {
-  return <div>ForgotPass</div>;
+  const [email, setEmail] = useState("");
+
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {};
+
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot Password</p>
+      </header>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            id="email"
+            className="emailInput"
+            placeholder="Email"
+            value={email}
+            onChange={handleChange}
+          />
+          <Link className="forgotPasswordLink" to="/sign-in">
+            Sign In
+          </Link>
+
+          <div className="signInBar">
+            <div className="signInText">Send Reset Link</div>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#fff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default ForgotPassword;
