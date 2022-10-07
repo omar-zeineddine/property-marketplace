8fbe548bdca7a2de268aa520f65ed9aff863f831
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-07 10:33:01 +0300

    feat - add useAuthState hook

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
+import useAuthStatus from "../hooks/useAuthStatus";
 
 const PrivateRoute = () => {
-  const loggedIn = false;
+  const { loggedIn, checkingStatus } = useAuthStatus();
+
+  if (checkingStatus) {
+    return <h4>loading...</h4>;
+  }
+
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 };
 
