02d41bdd9c2a5ac366d4b40e7dd48d64770f01fd
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-07 10:26:51 +0300

    feat - add private route component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
