394d11e0d4ec30c228da442b7ee96da12abfe397
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-08 22:53:45 +0300

    feat - conditional render of listing details

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -7,13 +7,44 @@ const ListingItem = ({ listing, id }) => {
   return (
     <div>
       <li className="categoryListing">
-        <Link to={`/category/${listing.type}/${id}`}>
-          <div className="categoryListingLink">
-            <img
-              className="categoryListingImg"
-              src={listing.imageUrls[0]}
-              alt={listing.name}
-            />
+        <Link
+          to={`/category/${listing.type}/${id}`}
+          className="categoryListingLink"
+        >
+          <img
+            src={listing.imageUrls[0]}
+            alt={listing.name}
+            className="categoryListingImg"
+          />
+          <div className="categoryListingDetails">
+            <p className="categoryListingLocation">{listing.location}</p>
+            <p className="categoryListingName">{listing.name}</p>
+
+            <p className="categoryListingPrice">
+              $
+              {listing.offer
+                ? listing.discountedPrice
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                : listing.regularPrice
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              {listing.type === "rent" && " / Month"}
+            </p>
+            <div className="categoryListingInfoDiv">
+              <img src={bedIcon} alt="bed" />
+              <p className="categoryListingInfoText">
+                {listing.bedrooms > 1
+                  ? `${listing.bedrooms} Bedrooms`
+                  : "1 Bedroom"}
+              </p>
+              <img src={bathtubIcon} alt="bath" />
+              <p className="categoryListingInfoText">
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} Bathrooms`
+                  : "1 Bathroom"}
+              </p>
+            </div>
           </div>
         </Link>
       </li>
