b2df097e2693a62914f9d4db1120dccd163bc50a
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-08 23:09:21 +0300

    feat - add link in profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,8 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { useNavigate, Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
+import homeIcon from "../assets/svg/homeIcon.svg";
 
 const Profile = () => {
   const auth = getAuth();
@@ -96,6 +98,11 @@ const Profile = () => {
               />
             </form>
           </div>
+          <Link to="/create-listing" className="createListing">
+            <img src={homeIcon} alt="home" />
+            <p>Sell or rent your property</p>
+            <img src={arrowRight} alt="arrow right" />
+          </Link>
         </main>
       </div>
     </>
