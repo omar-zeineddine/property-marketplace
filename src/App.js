c6ec86bcbe86507166f2d7328ccff8c3af099f26
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-10 17:10:03 +0300

    feat - add single listing component and route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Offers from "./pages/Offers";
 import Category from "./pages/Category";
 import Profile from "./pages/Profile";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -27,6 +28,10 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
         </Routes>
         <Navbar />
       </Router>
