43ec17fcdebe0e553cbceac63c3de3a6b4e97802
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-10 18:09:02 +0300

    feat - add form and mail to property owner link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,7 +15,6 @@ const Contact = () => {
     const getLandlord = async () => {
       const docRef = doc(db, "users", params.landlordId);
       const docSnap = await getDoc(docRef);
-      //   console.log(docSnap);
 
       if (docSnap.exists()) {
         setLandlord(docSnap.data());
@@ -33,6 +32,39 @@ const Contact = () => {
       <header>
         <p className="pageHeader">Contact Landlord</p>
       </header>
+
+      {landlord !== null && (
+        <main>
+          <div className="contactLandlord">
+            <p className="landlordName">Contact {landlord?.name}</p>
+          </div>
+
+          <form className="messageForm">
+            <div className="messageDiv">
+              <label htmlFor="message" className="messageLabel">
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="textarea"
+                value={message}
+                onChange={onChange}
+              ></textarea>
+            </div>
+
+            <a
+              href={`mailto:${landlord.email}?Subject=${searchParams.get(
+                "listingName"
+              )}&body=${message}`}
+            >
+              <button type="button" className="primaryButton">
+                Send Message
+              </button>
+            </a>
+          </form>
+        </main>
+      )}
     </div>
   );
 };
