3ea59aea398c7071e51b93c9a31fc25c1c9613c1
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-10 18:06:06 +0300

    feat - get property creator data

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const [message, setMessage] = useState("");
+  const [landlord, setLandlord] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const params = useParams();
+
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, "users", params.landlordId);
+      const docSnap = await getDoc(docRef);
+      //   console.log(docSnap);
+
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data());
+      } else {
+        toast.error("Could not get landlord data");
+      }
+    };
+    getLandlord();
+  }, [params.landlordId]);
+
+  const onChange = (e) => setMessage(e.target.value);
+
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Contact Landlord</p>
+      </header>
+    </div>
+  );
 };
 
 export default Contact;
